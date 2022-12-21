c6e5f00a8f1e70bed28f21e5bf57bacbdd24aced
wlee221 <willeea@amazon.com>
2022-12-20 19:28:11 -0800

    Actually call the function

--- packages/ui/src/helpers/experiment.ts
+++ packages/ui/src/helpers/experiment.ts
@@ -5,5 +5,10 @@ import '@tensorflow/tfjs-backend-webgl';
 import '@tensorflow/tfjs-backend-cpu';
 
 export default () => {
-  console.log(tf.ready, tf.setBackend, blazeface.load, tfjsWasm.setWasmPaths);
+  console.log(
+    tf.ready(),
+    tf.setBackend('experiment'),
+    blazeface.load({}),
+    tfjsWasm.setWasmPaths('experiment')
+  );
 };
