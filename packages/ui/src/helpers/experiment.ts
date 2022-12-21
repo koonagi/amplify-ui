02d87337d31794146df93ec821cad658d226143d
wlee221 <willeea@amazon.com>
2022-12-20 18:32:04 -0800

    Add entry path

--- packages/ui/src/helpers/experiment.ts
+++ packages/ui/src/helpers/experiment.ts
@@ -1,7 +1,9 @@
 import * as tf from '@tensorflow/tfjs-core';
 import * as blazeface from '@tensorflow-models/blazeface';
-
-// TODO:: Figure out if we should lazy load these or not.
 import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 import '@tensorflow/tfjs-backend-webgl';
 import '@tensorflow/tfjs-backend-cpu';
+
+export default () => {
+  console.log(tf.ready, tf.setBackend, blazeface.load, tfjsWasm.setWasmPaths);
+};
--- packages/ui/src/helpers/index.ts
+++ packages/ui/src/helpers/index.ts
@@ -2,3 +2,4 @@ export * from './authenticator';
 export * from './accountSettings';
 export * from './storage';
 export { getLogger } from './utils';
+export { default as experiment } from './experiment';
